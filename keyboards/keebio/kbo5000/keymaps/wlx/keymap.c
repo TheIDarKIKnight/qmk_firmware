a312afe69bb2f987a09869a3fcbe4e0a753e795c
wolf Lalaux <10999361+TheIDarKIKnight@users.noreply.github.com>
2023-10-18 09:21:15 +0200

    Maj 18/10/23

--- keyboards/keebio/kbo5000/keymaps/wlx/keymap.c
+++ keyboards/keebio/kbo5000/keymaps/wlx/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F14,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_PGDN,
     KC_F15,  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_HOME, KC_END,
     KC_F16,  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,          KC_UP,
-    KC_F17,  KC_LCTL, KC_LGUI, MO(1), KC_LALT,   KC_SPC,  KC_SPC,                    MO(1),   KC_SPC,  KC_RALT, KC_MENU,                   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F17,  KC_LCTL, KC_LGUI, TG(1), KC_LALT,   KC_SPC,  KC_SPC,                    MO(1),   KC_SPC,  KC_RALT, KC_MENU,                   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [1] = LAYOUT_ansi(
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_MOD, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P7, KC_P8, KC_P9, _______, _______, _______, _______, _______,
     KC_MPLY, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P4, KC_P5, KC_P6, _______,          _______, _______, _______,
     KC_EJCT, RGB_MODE_XMAS,          _______, _______, _______, _______, _______,          _______, _______, KC_P1, KC_P2, KC_P3,          _______,          _______,
-    KC_MUTE, RGB_M_K, MO(2), _______, _______, _______, _______,                   _______, _______, KC_P0, _______,                   _______, _______, _______, _______
+    KC_MUTE, RGB_M_K, MO(2), TG(0), _______, _______, _______,                   _______, _______, KC_P0, _______,                   _______, _______, _______, _______
   ),
 
   [2] = LAYOUT_ansi(
